6028302c7b5df7a317b81feb14ad73f0ce290101
Mark <864345787@qq.com>
2019-12-30 16:29:08 +0800

    add project

--- PytorchPhotosSortAndResize.py
+++ PytorchPhotosSortAndResize.py
@@ -5,6 +5,8 @@ if __name__ == '__main__':
     TrainData=TrainPhotos('./train')
     TestData=TestPhotos('./test')
     print(TrainData[2],'\n')
-    print(TestData.__getitem__(2).size())  
-    save_image(TrainData[2], './dc_img/aaa.png')
+    print(TestData.__getitem__(2).size()) 
+    
+    for i in range(TestData.__len__()):
+        save_image(TestData[i], './dc_img/{}.png'.format(i+25))
    
--- dataloader.py
+++ dataloader.py
@@ -5,8 +5,10 @@ from PIL import Image
 import numpy as np
 from torchvision import transforms
 
-transform = transforms.Compose([
-    transforms.ToTensor(),  # 将图片转换为Tensor,归一化至[0,1] 
+transform = transforms.Compose([ 
+    #transforms.Resize((60,100)),                       #注意Totensor()必须在Resize()后，否则会报错
+    transforms.ToTensor(),                            # 将图片转换为Tensor,归一化至[0,1]
+    #transforms.ToPILImage(),
 ])
 
 #定义自己的train数据集合
@@ -21,6 +23,7 @@ class TrainPhotos(data.Dataset):
     def __getitem__(self, index):
         img_path = self.imgs[index]
         pil_img = Image.open(img_path)
+     #   pil_img = pil_img.crop((0,0,720,430))       #裁剪
         if self.transforms:
             data = self.transforms(pil_img)
         else:
@@ -36,13 +39,16 @@ class TestPhotos(data.Dataset):
     def __init__(self,root):
         # 所有图片的绝对路径
         imgs=os.listdir(root)
-        imgs.sort(key=lambda x:int(x[:-4]))      #图片名从小到大排序
+        print(imgs)
+        imgs.sort(key=lambda x:int(x.split('.')[-2].split('-')[-1]))     #图片名从小到大排序
+        print(imgs)
         self.imgs=[os.path.join(root,k) for k in imgs]
         self.transforms=transform
 
     def __getitem__(self, index):
         img_path = self.imgs[index]
         pil_img = Image.open(img_path)
+   #     pil_img = pil_img.crop((0,0,720,430))      #裁剪
         if self.transforms:
             data = self.transforms(pil_img)
         else:
